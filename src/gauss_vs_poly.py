4949646db3638f32810a26230a03468e1733ec15
Emil Martens <emil.martens@gmail.com>
2023-12-06 19:04:07 +0000

    .

--- src/gauss_vs_poly.py
+++ src/gauss_vs_poly.py
@@ -7,18 +7,25 @@ x+0.5
 
 """
 def gauss(x):
-    return np.exp(-(3*x)**2)
+    return np.exp(-(x)**2)
 
 def poly1(x):
+    x=x/3
     return 1-x**2
 
 def poly2(x):
+    x=x/3
     return (1-x**2)**2
 
 def poly4(x):
+    x=x/3
     return (x**2-1)**4
 
+def poly8(x):
+    x=x/3
+    return (x**2-1)**8
 def polyd(x):
+    x=x/3
     return (1-x**2)**2 * (1+x**2)
 
 def combind(fa, fb, fk):
@@ -27,11 +34,12 @@ def combind(fa, fb, fk):
 
 f = combind(poly1, poly4, poly4)
 
-t = np.linspace(-1.0, 1.0, 1000)
+t = np.linspace(-3.0, 3.0, 1000)
 plt.plot(t, gauss(t), label='$e^{-(3x)^2}$')
+plt.plot(t, poly8(t), label='$(1-x^2)^8$')
+plt.plot(t, poly4(t), label='$(1-x^2)^4$')
+plt.plot(t, poly2(t), label='$(1-x^2)^2$')
 plt.plot(t, poly1(t), label='$1-x^2$')
-plt.plot(t, poly2(t), label='$1-x^2)^2$')
-# plt.plot(t, poly4(t), label='$(1-x^2)^4$')
 # plt.plot(t, polyd(t), label='$(1-x^2)^2(1+x^2)$')
 # plt.plot(t, polyd(t), label='$(1-x^2)^2(1+x^2)$')
 plt.legend()
